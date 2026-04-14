3503f54eb008daf68b58debd70d0ee74e09d3bde
leolorenzato <lorenzatoleonardo@gmail.com>
2026-04-14 17:54:30 +0200

    Refactor items default

--- internal/items/default.go
+++ internal/items/default.go
@@ -1,45 +1,59 @@
 package items
 
-const (
-	defaultLockCmd      string = "loginctl lock-session"
-	defaultShutdownCmd  string = "shutdown -h now"
-	defaultRebootCmd    string = "shutdown -r now"
-	defaultLogoutCmd    string = "loginctl terminate-session $XDG_SESSION_ID"
-	defaultSuspendCmd   string = "systemctl suspend"
-	defaultHibernateCmd string = "systemctl hibernate"
-)
+func DefaultOrder() []ItemId {
+	return []ItemId{
+		lockId,
+		shutdownId,
+		rebootId,
+		logoutId,
+		suspendId,
+		hibernateId,
+	}
+}
+
+func DefaultCmds() map[ItemId]string {
+	return map[ItemId]string{
+		lockId:      "loginctl lock-session",
+		shutdownId:  "shutdown -h now",
+		rebootId:    "shutdown -r now",
+		logoutId:    "loginctl terminate-session $XDG_SESSION_ID",
+		suspendId:   "systemctl suspend",
+		hibernateId: "systemctl hibernate",
+	}
+}
 
 func DefaultCfg() Cfg {
+	defaultCmds := DefaultCmds()
 	return Cfg{
 		Lock: LockCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultLockCmd,
+			Cmd:  defaultCmds[lockId],
 		},
 		Shutdown: ShutdownCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultShutdownCmd,
+			Cmd:  defaultCmds[shutdownId],
 		},
 		Reboot: RebootCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultRebootCmd,
+			Cmd:  defaultCmds[rebootId],
 		},
 		Logout: LogoutCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultLogoutCmd,
+			Cmd:  defaultCmds[logoutId],
 		},
 		Suspend: SuspendCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultSuspendCmd,
+			Cmd:  defaultCmds[suspendId],
 		},
 		Hibernate: HibernateCfg{
 			Show: true,
 			Icon: "",
-			Cmd:  defaultHibernateCmd,
+			Cmd:  defaultCmds[hibernateId],
 		},
 	}
 }
